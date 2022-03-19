3402b9e3008888a7440cdeae4b0111968e98ccb2
iambasanta <sangpangbasanta@gmail.com>
2022-03-19 20:28:49 +0545

    WIP for admin user

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -2,18 +2,21 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        User::factory()->create([
+            'email' => 'super@admin.com'
+        ]);
     }
 }
