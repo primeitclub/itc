9215451f4fd20b16afe40b75febc54b3466e53ab
iambasanta <sangpangbasanta@gmail.com>
2022-04-10 08:39:13 +0545

    Show notification on top right corner

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,6 +1,6 @@
 @if(session('success'))
 <div x-data="{ open:true }" x-show="open" x-init="setTimeout(()=>open=false,3000)">
-    <div class="fixed bottom-0 right-0 z-20 flex justify-between w-full max-w-sm px-6 py-5 mx-6 my-8 bg-white border shadow-lg rounded-xl">
+    <div class="fixed top-0 right-0 z-20 flex justify-between w-full max-w-sm px-6 py-5 mx-6 my-8 bg-white border shadow-lg rounded-xl">
         <div class="flex items-center ">
             <svg class="w-5 h-5 text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
