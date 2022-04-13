116f4e6213e0b189c59dfb8d6256180f3a3f5bc4
iambasanta <sangpangbasanta@gmail.com>
2022-04-13 19:24:08 +0545

    Create Blog Category seeder

---
+++ database/seeders/BlogCategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BlogCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        BlogCategory::create([
+            'title' => 'Uncategorized',
+            'slug' => 'uncategorized'
+        ]);
+    }
+}
