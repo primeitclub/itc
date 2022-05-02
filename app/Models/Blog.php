a1de8cd5bdae478b463d730e2eb79e801a1a6a46
iambasanta <sangpangbasanta@gmail.com>
2022-05-02 19:50:24 +0545

    Show blog's publication status

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -20,8 +20,26 @@ class Blog extends Model
         'published_at'
     ];
 
+    protected $dates = ['published_at'];
+
     public function blogCategory()
     {
         return $this->belongsTo(BlogCategory::class);
+    }
+
+    public function dateFormatted()
+    {
+        return $this->created_at->format('d-m-Y');
+    }
+
+    public function publicationStatus()
+    {
+        if (!$this->published_at) {
+            return  '<span class="px-2 py-1 font-semibold leading-tight text-yellow-700 bg-yellow-100 rounded-full ">Draft</span>';
+        } elseif ($this->published_at && ($this->published_at->isFuture())) {
+            return  '<span class="px-2 py-1 font-semibold leading-tight text-blue-700 bg-blue-100 rounded-full ">Scheduled</span>';
+        } else {
+            return  '<span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">Published</span>';
+        }
     }
 }
--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -35,10 +35,7 @@
                             </td>
 
                             <td class="px-4 py-3 text-xs">
-                                2022-04-14 |
-                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full ">
-                                    Published
-                                </span>
+                                {{ $blog->dateFormatted() }} | {!! $blog->publicationStatus() !!}
                             </td>
 
                             <td class="px-4 py-3">
