d65f83941be7dbc9f7762e991c12921f8bd2b3d5
iambasanta <sangpangbasanta@gmail.com>
2022-05-06 20:30:51 +0545

    Remove UNIQUE constraint from blog category title

--- database/migrations/2022_04_11_143135_create_blog_categories_table.php
+++ database/migrations/2022_04_11_143135_create_blog_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title')->unique();
+            $table->string('title');
             $table->string('slug')->unique();
             $table->timestamps();
         });
