7f747621b0b955558ae357748151d45ce06f99df
iambasanta <sangpangbasanta@gmail.com>
2022-05-07 20:30:21 +0545

    Create Event Category Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(BlogCategoriesTableSeeder::class);
+        $this->call(EventCategoriesTableSeeder::class);
     }
 }
---
+++ database/seeders/EventCategoriesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EventCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EventCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        EventCategory::create([
+            'title' => 'Uncategorized',
+            'slug' => 'uncategorized'
+        ]);
+    }
+}
