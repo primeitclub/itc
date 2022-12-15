da319ef010a56187b528bc4d9cf8de71740390b1
iambasanta <sangpangbasanta@gmail.com>
2022-12-15 19:47:52 +0545

    WIP for alumni testimonial management

---
+++ app/Models/AlumniTestimonial.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AlumniTestimonial extends Model
+{
+    use HasFactory;
+}
