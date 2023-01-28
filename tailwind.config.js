cfdaa05c43d4fbca06dafb9d5d8149abeb8df45c
iambasanta <sangpangbasanta@gmail.com>
2023-01-28 19:30:15 +0545

    Create and use radial gradient background

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,12 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                "rgradient-blue":
+                    "radial-gradient(56.49% 353.68% at 52.15% 46.49%, #3188EF 0%, #2B88DE 0.01%, #123ABC 100%)",
+            },
+        },
     },
-    plugins: [
-        require('@tailwindcss/forms'),
-    ],
-}
+    plugins: [require("@tailwindcss/forms")],
+};
