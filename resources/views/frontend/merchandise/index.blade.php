b22fe6a8afb05f920b534c55d805655f427709a0
iambasanta <sangpangbasanta@gmail.com>
2023-03-26 17:28:52 +0545

    Enhance merchandise page

--- resources/views/frontend/merchandise/index.blade.php
+++ resources/views/frontend/merchandise/index.blade.php
@@ -5,59 +5,54 @@
         </div>
     </x-frontend.wave>
     <!-- merch content starts -->
-    <section class=" pt-24 sm:py-20">
-        <div class="p-14 container mx-auto">
-            <div class=" sm:mx-12 lg:mx-24 mb-4">
-                @if(!$merchandises->count())
-                <h1 class="text-center text-2xl sm:text-4xl text-primary font-bold">Oops! No content to show!</h1>
-                <p class="text-center text-sm sm:text-xl text-greish">We will launch our merchandise soon...</p>
-                @else
-                <h1 class="text-center text-2xl sm:text-4xl text-primary font-bold">Featured Items</h1>
-                <p class="text-center text-sm sm:text-xl text-greish">Browse our collection of different merchandise</p>
-                <div class=" ">
-                    @foreach($merchandises as $merchandise)
-                    <div class="mt-8 rounded-xl bg-white w-full sm:w-auto drop-shadow-xl hover:drop-shadow-2xl">
-                        <div class="grid grid-cols-1 md:grid-cols-2 p-8 ">
-            
-                                <figure class="pb-4">
-                                    <img class="h-full" src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" alt="" />
-                                </figure>
-                            
-                            <div class="pl-8 ">
-                                <h3 class="text-primary poppins text-4xl font-semibold tracking-wide">
-                                    {{ $merchandise->title }}
-                                </h3>
-                                <div class="list-none">
-                                    <ol>
-                                        <li class=" pt-6  flex">
-                                            <p class=" poppins text-3xl leading-3 font-semibold tracking-wide">Rs. {{ $merchandise->price }}</p>
-                                            <p class="text-gray-500 text-sm ml-auto"> Available</p>
-                                        </li>
+    <section class="mt-10 sm:mt-24 mb-10 sm:mb-24 max-w-7xl mx-auto px-8 sm:px-6">
+        @if(!$merchandises->count())
+        <h1 class="text-center text-2xl sm:text-4xl text-primary font-bold">Oops! No content to show!</h1>
+        <p class="text-center text-sm sm:text-xl text-greish">We will launch our merchandise soon...</p>
+        @else
+        <h1 class="text-center text-2xl sm:text-4xl text-primary font-bold">Featured Items</h1>
+        <p class="text-center text-sm sm:text-xl text-greish">Browse our collection of different merchandise</p>
+        <div class=" ">
+            @foreach($merchandises as $merchandise)
+            <div class="mt-8 rounded-xl bg-white w-full sm:w-auto drop-shadow-xl hover:drop-shadow-2xl">
+                <div class="grid grid-cols-1 md:grid-cols-2 p-4 sm:p-16">
 
-                                    </ol>
-                                </div>
-                                <div class="pt-6">
-                                    <p class="text-xl "> <span class="text-greish">Sizes:</span> XL/XXL </p>
-                                    <p class="text-xl "> <span class="text-greish">Color: </span> Black </p>
-                                </div>
-                                <div class="pt-6  text-greish h-60 overflow-auto">
-                                    <p class="py-2 text-base font-light leading-6 ">
-                                        {!! $merchandise->description !!}
-                                    </p>
-                                </div>
-                                <button class="primary rounded-3xl px-20 py-2 sm:text-xl mb:text-lg text-white bg-primary hover:bg-blue-900">
-                                    <a href="{{ $merchandise->form_link }}">
-                                        Order now
-                                    </a>
-                                </button>
-                            </div>
+                    <figure class="">
+                        <img class="max-auto-526 w-full h-full" src="{{ $merchandise->imageUrl() }}" alt="" />
+                    </figure>
+
+                    <div class="pl-8 ">
+                        <!-- <h3 class="text-primary poppins text-4xl font-semibold tracking-wide"> -->
+                        <h3 class="mt-8 sm:mt-0 text-2xl sm:text-4xl font-semibold text-primary tracking-wide">
+                            {{ $merchandise->title }}
+                        </h3>
+                        <div class="list-none">
+                            <ol>
+                                <li class="pt-6 flex flex-col sm:flex-row">
+                                    <p class="text-greish text-3xl leading-3 font-semibold tracking-wide">Rs. {{ $merchandise->price }}</p>
+                                    <span class="text-green-700 bg-green-200 text-sm ml-auto rounded-full px-2 py-0.5"> Available</span>
+                                </li>
+                            </ol>
                         </div>
+                        <div class="pt-6">
+                            <p class="text-xl "> <span class="text-greish">Sizes:</span> XL/XXL </p>
+                            <p class="text-xl "> <span class="text-greish">Color: </span> Black </p>
+                        </div>
+                        <div class="pt-6 text-greish mb-6">
+                            <p class="py-2 text-base font-light leading-6 ">
+                                {!! $merchandise->description !!}
+                            </p>
+                        </div>
+                        <button class="button py-3 px-20 rounded-3xl">
+                            <a href="{{ $merchandise->form_link }}">
+                                Order now
+                            </a>
+                        </button>
                     </div>
-                    @endforeach
                 </div>
-
-                @endif
             </div>
+            @endforeach
         </div>
+        @endif
     </section>
 </x-frontend.app>
