4d666af27e89cda6c9bdb549bccc2534b8c7f3a8
iambasanta <sangpangbasanta@gmail.com>
2023-04-22 20:18:09 +0545

    List executive members by designation

--- app/Http/Controllers/MembersPageController.php
+++ app/Http/Controllers/MembersPageController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class MembersPageController extends Controller
 {
     public function index() {
-        $executiveMembers = Member::ExecutiveMember()->where('batch',date('Y'))->orderBy('name')->get();
+        $executiveMembers = Member::ExecutiveMember()->OrderByDesignation()->where('batch', date('Y'))->get();
         $generalMembers = Member::GeneralMember()->where('batch',date('Y'))->orderBy('name')->get();
         return view('frontend.members', compact('executiveMembers', 'generalMembers'));
     }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -54,4 +54,20 @@ class Member extends Model
     public function scopeGeneralMember($query) {
         return $query->where('type', 'General');
     }
+
+    public function scopeOrderByDesignation($query) {
+        return $query->orderByRaw("
+            CASE
+            WHEN Designation = 'President' THEN 1
+            WHEN Designation = 'Vice President' THEN 2
+            WHEN Designation = 'Secretary' THEN 3
+            WHEN Designation = 'Event Management Director' THEN 4
+            WHEN Designation = 'External Relationship Director' THEN 5
+            WHEN Designation = 'Finance Director' THEN 6
+            WHEN Designation = 'Human Resource Director' THEN 7
+            WHEN Designation = 'Marketing Director' THEN 8
+            ELSE Designation
+            END
+            ");
+    }
 }
